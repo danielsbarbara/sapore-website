0ecbd99e8520c3040c730d81ceb69f92e8f683f3
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-09-25 06:45:52 +0100

    uri mongo correction

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="bg-sapore-room h-full w-full bg-fixed bg-cover bg-center 
     bg-no-repeat overflow-scroll">
         <div className="text-white py-2 px-3 bg-gradient-to-t from-black/50
-        h-[25rem] md:h-[40rem] flex flex-col justify-end">
+        h-[28rem] md:h-[40rem] flex flex-col justify-end">
           <p className="text-xl font-bold md:text-3xl">Sapore</p>
           <Texts text="imageText" styles="md:text-xl" />
           <Texts text="slogan" styles="italic font-thin md:text-xl" />
