55e82b0dc1a7b2c283a8a5c27fda8ec658590802
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-10-12 18:59:28 +0100

    test

--- app/server/mongoConnect.ts
+++ app/server/mongoConnect.ts
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 
-const url: string = 'mongodb://localhost:27017/'
+const url: string = 'mongodb+srv://danielsantosbarbara:sapore2024@cluster0.5svdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 export const DBname: string = 'sapore'
 
 let client: any = undefined
