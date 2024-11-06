97cdc69fbc05999bfadb670e72f80699f5e25b52
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-11-06 21:35:40 +0000

    Ajust footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Texts } from "./Texts"
 export const Footer: React.FC = () => {
     return (
         <div className="w-full bg-layoutColor">
-            <div className="h-[15rem] w-full py-4 text-white flex flex-col 
+            <div className="h-[25rem] md:h-[15rem] w-full py-4 text-white flex flex-col 
         items-center gap-8 md:flex-row md:justify-around">
                 <div>
                     <Texts text="footer1" styles="text-xl font-bold py-3" />
@@ -26,7 +26,7 @@ export const Footer: React.FC = () => {
                     <p>Av. Sidónio Pais, nº2 - B 1050-214 - Lisboa</p>
                 </div>
             </div>
-            <Texts text="footer2" styles="py-3 font-bold text-sm justify-self-center text-white" />
+            <Texts text="footer2" styles="py-3 font-bold text-sm text-white justify-self-center" />
         </div>
     )
 }
